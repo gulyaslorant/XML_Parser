4c529874fc724784856c0d3078538f867131eff0
Gulyas Lorant <gulyas.lorant@gmail.com>
2023-02-20 14:38:10 +0100

    2. Spalte mit Bildtitel eingefügt

--- Parser.py
+++ Parser.py
@@ -7,6 +7,24 @@ tree = ET.parse('Sourcefiles/Cloudberry.xml')
 # Get the root element
 root = tree.getroot()
 
+image_urls = []
+
+# Iterate over each 'item' element in the XML file
+for item in root.findall('./channel/item'):
+    # Get the 'content' element
+    title = item.find('title').text
+    # If the 'content' element exists and has a 'url' attribute
+    if title is not None:
+        # Add the URL to the list of image URLs
+        image_urls.append(title)
+
+# Create a new Excel workbook and sheet
+wb = Workbook()
+ws = wb.active
+
+# Write the image URLs to the Excel sheet
+for i, title  in enumerate(image_urls):
+    ws.cell(row=i+1, column=1, value=title)
 # Define a list to store the image URLs
 image_urls = []
 
@@ -24,12 +42,11 @@ for item in root.findall('./channel/item'):
 print(image_urls)
 
 # Create a new Excel workbook and sheet
-wb = Workbook()
-ws = wb.active
 
 # Write the image URLs to the Excel sheet
 for i, url in enumerate(image_urls):
-    ws.cell(row=i+1, column=1, value=url)
+    ws.cell(row=i+1, column=2, value=url)
 
 # Save the Excel file
 wb.save('Sourcefiles/Bildurl.xlsx')
+
